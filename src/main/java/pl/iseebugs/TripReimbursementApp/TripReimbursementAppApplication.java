bb68ca2fef71658be215265b71056550061399cd
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-11-03 08:57:39 +0100

    Start project

---
+++ src/main/java/pl/iseebugs/TripReimbursementApp/TripReimbursementAppApplication.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.TripReimbursementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripReimbursementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripReimbursementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/iseebugs/TripReimbursementApp/TripReimbursementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.TripReimbursementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripReimbursementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
