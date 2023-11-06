e0d41778e85c091fd5b5a0cb1699d040827db5ea
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-11-06 10:25:49 +0100

    Add behavioral scenerios for updateUserGroupById

--- src/test/java/pl/iseebugs/TripReimbursementApp/logic/UserGroupServiceTest.java
+++ src/test/java/pl/iseebugs/TripReimbursementApp/logic/UserGroupServiceTest.java
@@ -1,24 +1,42 @@
 package pl.iseebugs.TripReimbursementApp.logic;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserGroupServiceTest {
 
-    @Test
+/*    @Test
     void readAll() {
     }
 
     @Test
     void createUserGroup() {
+    }*/
+
+    @Test
+    @DisplayName("should throw UserGroupNotFoundException when given id not found")
+    void updateUserGroupById_noUserGroup_throwsUserGroupNotFoundException() {
     }
 
     @Test
-    void updateUserGroupById() {
+    @DisplayName("should throw IllegalArgumentException when given name already exists")
+    void updateUserGroupById_userGroupsNameAlreadyExists_throwsIllegalArgumentException() {
     }
 
     @Test
+    @DisplayName("should throw IllegalArgumentException when given name is empty or has only white marks")
+    void updateUserGroupById_emptyNameParam_throwsIllegalArgumentException() {
+    }
+
+    @Test
+    @DisplayName("should rename user group")
+    void updateUserGroupById_updatesUserGroup() {
+    }
+
+
+    /*@Test
     void deleteUserGroup() {
-    }
+    }*/
 }
