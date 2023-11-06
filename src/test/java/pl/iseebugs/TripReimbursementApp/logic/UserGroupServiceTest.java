f8a3b8d3d46b1bbb4febea48922bf9424632b39b
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-11-06 09:39:54 +0100

    Add UserGroupServiceTest file

---
+++ src/test/java/pl/iseebugs/TripReimbursementApp/logic/UserGroupServiceTest.java
@@ -0,0 +1,24 @@
+package pl.iseebugs.TripReimbursementApp.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGroupServiceTest {
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void createUserGroup() {
+    }
+
+    @Test
+    void updateUserGroupById() {
+    }
+
+    @Test
+    void deleteUserGroup() {
+    }
+}
