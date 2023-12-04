b3185700a7362223634e58591dd279035ed8917a
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-12-04 23:22:43 +0100

    Add read model

---
+++ src/main/java/pl/iseebugs/TripReimbursementApp/model/projection/ReceiptTypeReadModelShort.java
@@ -0,0 +1,30 @@
+package pl.iseebugs.TripReimbursementApp.model.projection;
+
+import pl.iseebugs.TripReimbursementApp.model.ReceiptType;
+
+public class ReceiptTypeReadModelShort {
+    private int id;
+    private String name;
+    private double maxValue;
+
+    public ReceiptTypeReadModelShort() {
+    }
+
+    public ReceiptTypeReadModelShort(ReceiptType receiptType) {
+        id = receiptType.getId();
+        name = receiptType.getName();
+        maxValue = receiptType.getMaxValue();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMaxValue() {
+        return maxValue;
+    }
+}
