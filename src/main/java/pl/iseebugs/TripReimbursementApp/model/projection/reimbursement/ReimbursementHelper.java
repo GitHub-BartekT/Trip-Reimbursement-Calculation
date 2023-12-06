2ecac8f070ace516835f8d310749ad27d560b1f2
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-12-06 23:27:45 +0100

    Make ReimbursementHelper

--- src/main/java/pl/iseebugs/TripReimbursementApp/model/projection/reimbursement/ReimbursementHelper.java
+++ src/main/java/pl/iseebugs/TripReimbursementApp/model/projection/reimbursement/ReimbursementHelper.java
@@ -1,10 +1,13 @@
 package pl.iseebugs.TripReimbursementApp.model.projection.reimbursement;
 
 import pl.iseebugs.TripReimbursementApp.model.Reimbursement;
+import pl.iseebugs.TripReimbursementApp.model.UserCost;
 import pl.iseebugs.TripReimbursementApp.model.UserGroup;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReimbursementHelper {
     protected static double returnValue(Reimbursement reimbursement) {
@@ -15,6 +18,16 @@ public class ReimbursementHelper {
         double dailyAllowance = userGroup.getDailyAllowance();
         double maxRefund = userGroup.getMaxRefund();
         long duration;
+        Set<UserCost> userCosts = new HashSet<>();
+        double allCostsSum = 0;
+
+        if (reimbursement.getUserCosts() != null) {
+            userCosts.addAll(reimbursement.getUserCosts());
+            for (UserCost userCost : userCosts) {
+                allCostsSum += Math.min(userCost.getCostValue(), userCost.getReceiptType().getMaxValue());
+            }
+        }
+
         LocalDate start = reimbursement.getStartDate();
         LocalDate end = reimbursement.getEndDate();
 
@@ -26,7 +39,7 @@ public class ReimbursementHelper {
         } else {
             duration = ChronoUnit.DAYS.between(start,end) + 1;
         }
-        double result = (duration * dailyAllowance) + (distance * costPerKm);
+        double result = (duration * dailyAllowance) + (distance * costPerKm) + allCostsSum;
         if (result > maxRefund){
             result = maxRefund;
         }
